095bee06d776c8861194b3ca77ebd2248bd6db53
Piotr Foltyniewicz <piotr.foltyniewicz@gmail.com>
2024-04-03 19:29:59 +0200

    root

---
+++ root.asm
@@ -0,0 +1,32 @@
+bits    64
+default rel
+
+global  main
+
+extern  printf
+extern  scanf
+
+section .data
+    formatEndVal db '%f', 0
+
+section .bss
+    endValue    resq 1
+
+section .text
+    main:
+        sub     rsp, 8
+        mov     rcx, 0
+        lea     rsi, [endValue + 0]
+        lea     rdi, [formatEndVal]
+        mov     al, 0
+        call scanf wrt ..plt
+
+    loop:
+        cmp rcx, endValue
+        jg end
+        ; add rooting and printing the output
+        add rcx, 1
+    end:
+        add     rsp, 8
+        sub     rax, rax
+        ret
