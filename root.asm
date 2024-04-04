0d8c94d1ddc1c1d71262ec412dc73935854828ff
Piotr Foltyniewicz <piotr.foltyniewicz@gmail.com>
2024-04-04 11:23:02 +0200

    root not finished

--- root.asm
+++ root.asm
@@ -7,26 +7,40 @@ extern  printf
 extern  scanf
 
 section .data
-    formatEndVal db '%f', 0
+    format_in db '%lf', 0
+    format_out db '%lf ', 0xA, 0
+    start_value dq 0.0
+    step dq 0.125
 
 section .bss
-    endValue    resq 1
+    end_value    resq 1
 
 section .text
     main:
         sub     rsp, 8
-        mov     rcx, 0
-        lea     rsi, [endValue + 0]
-        lea     rdi, [formatEndVal]
+        lea     rsi, [end_value]
+        lea     rdi, [format_in]
         mov     al, 0
-        call scanf wrt ..plt
+        call    scanf wrt ..plt
+        movsd  xmm4, xmm0 ;end conditon
+        movlpd  xmm1, [start_value] ;iterator value
 
     loop:
-        cmp rcx, endValue
-        jg end
-        movq xmm0, rcx
-        ; add rooting and printing the output
-        add rcx, 1
+        ; floating point comparison
+        movsd xmm0, xmm4
+        cmpltsd xmm0, xmm1
+        movq    rax, xmm0
+        cmp     rax, 2
+        je      end
+        sqrtsd  xmm0, xmm1
+
+        lea     rdi, [format_out]
+        mov     al, 1
+        call    printf wrt ..plt
+        ; adding 0.125 to the xmm1
+        addsd   xmm1, 0.125
+        jmp     loop
+
     end:
         add     rsp, 8
         sub     rax, rax
